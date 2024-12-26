8b2b7756a5c7fe78c899ecb3785b7e5405587c36
Arman Bhattacharjee <66437768+quacky20@users.noreply.github.com>
2024-12-27 00:39:11 +0530

    Session 2 Commit

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ import { getDatabase,
     ref,
     push,
     onValue,
-    remove } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-database.js";
+    remove,
+    update } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-database.js";
 
 
 const firebaseConfig = {
@@ -12,12 +13,15 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig)
 const database = getDatabase(app)
-const undoneTaskRef = ref(database, "undoneTasks")
-const doneTaskRef = ref(database, "doneTasks")
+const tasksRef = ref(database, "tasks")
 
 const menuIcon = document.getElementById("icons")
+const menuList = document.getElementById("navmenu")
 const inputEl = document.getElementById("inputEl")
 const inputBtn = document.getElementById("inputBtn")
+const undoneUl = document.getElementById("undone")
+const doneUl = document.getElementById("done")
+const doneHeader = document.getElementById("doneHeader")
 
 menuIcon.addEventListener("click", function(){
     menuList.classList.toggle("open")
@@ -25,19 +29,95 @@ menuIcon.addEventListener("click", function(){
 
 inputBtn.addEventListener("click", function(){
     if (inputEl.value != ""){
-        push(undoneTaskRef, inputEl.value)
+        push(tasksRef, {
+            title: inputEl.value,
+            done: false
+        })
         inputEl.value = ""
     }
 })
 
-let checkBox = document.getElementById("taskCheckBox")
+function render(snapshot)
+{
+    const tasks = snapshot.val()
+    undoneUl.innerHTML = "";
+    doneUl.innerHTML = "";
 
-function taskDone(){
-    if (checkBox.checked != true){
-        checkBox.checked = true
+    let hasDoneTask = false;
+    let hasUndoneTask = false;
+
+    for (const taskId in tasks){
+        const task = tasks[taskId]
+        const li = document.createElement("li")
+        li.textContent = task.title
+        li.dataset.id = taskId
+
+        const deleteSpan = document.createElement("span")
+        deleteSpan.textContent = "\u00d7"
+        deleteSpan.classList.add("delete")
+        li.appendChild(deleteSpan)
+
+        if (task.done){
+            hasDoneTask = true
+            li.classList.add("checked")
+            doneUl.appendChild(li)
+        }
+        else {
+            hasUndoneTask = true
+            undoneUl.appendChild(li)
+        }
+
+        if(hasUndoneTask && !hasDoneTask){
+            doneUl.innerHTML = "Finish your tasks!"
+        }
+        if(hasDoneTask && !hasUndoneTask){
+            undoneUl.innerHTML = "All tasks done!"
+        }
     }
-    else{
-        checkBox.checked = false
-    }
-    
 }
+
+onValue(tasksRef, function(snapshot){
+    if (snapshot.exists()){
+        render(snapshot)
+    }
+    else {
+        undoneUl.innerHTML = "Add tasks!"
+        doneUl.innerHTML = "Add tasks to finish them!"
+    }
+})
+
+
+undoneUl.addEventListener("click", function(e){
+    const taskLi = e.target.closest("li")
+    if(!taskLi) return;
+
+    const taskId = taskLi.dataset.id
+
+    if (e.target.tagName === "LI"){
+        const taskRef = ref(database, `tasks/${taskId}`)
+        const isDone = e.target.classList.contains("checked")
+        update(taskRef, { done: !isDone})
+    }
+    else if (e.target.classList.contains("delete")){
+        const taskRef = ref(database, `tasks/${taskId}`)
+        remove(taskRef)
+    }
+})
+
+doneUl.addEventListener("click", function(e){
+    const taskLi = e.target.closest("li")
+    if(!taskLi) return;
+
+    const taskId = taskLi.dataset.id
+
+    if (e.target.tagName === "LI"){
+        const taskRef = ref(database, `tasks/${taskId}`)
+        const isDone = e.target.classList.contains("checked")
+        update(taskRef, { done: !isDone})
+    }
+    else if (e.target.classList.contains("delete")){
+        const taskRef = ref(database, `tasks/${taskId}`)
+        remove(taskRef)
+    }
+})
+
